8392634a5d13d66fb94877e7df6182be4e4c061a
Maxim Agarev <maximagarev@yandex.ru>
2023-04-26 12:25:27 +0400

    Category set ready

---
+++ Tracker/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+extension UIColor {
+    static var ypBackground: UIColor? { UIColor(named: "Background") }
+    static var ypBlack: UIColor? { UIColor(named: "Black") }
+    static var ypGray: UIColor? { UIColor(named: "Gray") }
+    static var ypLightGray: UIColor? { UIColor(named: "Light Gray") }
+    static var ypWhite: UIColor? { UIColor(named: "White") }
+    static var ypRed: UIColor? { UIColor(named: "Red") }
+    static var ypBlue: UIColor? { UIColor(named: "Blue") }
+}
+
+extension UIColor {
+    static let ypColorSelection: [UIColor] = [
+        UIColor(red: 0.99, green: 0.3, blue: 0.29, alpha: 1),
+        UIColor(red: 1, green: 0.53, blue: 0.12, alpha: 1),
+        UIColor(red: 0, green: 0.48, blue: 0.98, alpha: 1),
+        UIColor(red: 0.43, green: 0.27, blue: 1, alpha: 1),
+        UIColor(red: 0.2, green: 0.81, blue: 0.41, alpha: 1),
+        UIColor(red: 0.9, green: 0.43, blue: 0.83, alpha: 1),
+        UIColor(red: 0.98, green: 0.83, blue: 0.83, alpha: 1),
+        UIColor(red: 0.2, green: 0.65, blue: 1, alpha: 1),
+        UIColor(red: 0.27, green: 0.9, blue: 0.62, alpha: 1),
+        UIColor(red: 0.21, green: 0.2, blue: 0.49, alpha: 1),
+        UIColor(red: 1, green: 0.4, blue: 0.3, alpha: 1),
+        UIColor(red: 1, green: 0.6, blue: 0.8, alpha: 1),
+        UIColor(red: 0.96, green: 0.77, blue: 0.55, alpha: 1),
+        UIColor(red: 0.47, green: 0.58, blue: 0.96, alpha: 1),
+        UIColor(red: 0.51, green: 0.17, blue: 0.95, alpha: 1),
+        UIColor(red: 0.68, green: 0.34, blue: 0.85, alpha: 1),
+        UIColor(red: 0.55, green: 0.45, blue: 0.9, alpha: 1),
+        UIColor(red: 0.18, green: 0.82, blue: 0.35, alpha: 1)
+    ]
+}
